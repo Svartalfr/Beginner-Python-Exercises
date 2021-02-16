a45dfe2621d78eb28568361a50af1682af1159a8
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2021-02-16 17:07:12 +0300

    Add files via upload

---
+++ 04-firstdebugging.py
@@ -0,0 +1,14 @@
+#print("String "+" manipulation")
+#print('Hello'+" "+input('What is your name?')+"!")
+"""
+##input() will get user input in console
+## Then print () the word "Hello" and the user input"""
+
+print("Hello"+" "+ input("What is your name?")+"!")
+
+print(len(input("What's your name? ")))
+
+
+
+
+

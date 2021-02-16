bb6b792409ef9bec84614c500eb42f003924ed70
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2021-02-16 17:20:51 +0300

    Add files via upload

---
+++ 06-learnstring.py
@@ -0,0 +1,18 @@
+"""phrase= "My Lovely Cat"
+print(phrase.isupper())""" 
+"""#Gives false result because not all letters are capitalized."""
+#****************************************************************************************************
+"""#If we want to get True; we can do this
+
+phrase= "My Lovely Cat"
+print(phrase.upper().isupper())
+
+Here is it is True"""
+#*****************************************************************************************************
+"""phrase="My Lovely cat"
+print(len(phrase))
+
+Here how long the string is """
+#*****************************************************************************************************
+
+

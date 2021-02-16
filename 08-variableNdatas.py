19633c8c1845409378ce480f2c1cbed355114967
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2021-02-16 17:24:07 +0300

    Update 08-variableNdatas.py

--- 08-variableNdatas.py
+++ 08-variableNdatas.py
@@ -1,7 +1,7 @@
-character_name ="John"
-character_age =70
-print(f"There is a guy called {character_name} , ")
-print(f"he was {character_age} years old.")
-character_name="Mike"
-print(f"He liked being called as {character_name} ,")
-print(f"but he didnt like being at {character_age}.")
+characterName ="Albert"
+characterAge =18
+print(f"There is a guy called {characterName} , ")
+print(f"he was {characterAge} years old.")
+characterName="Daniel"
+print(f"He liked being called as {characterName} ,")
+print(f"but he didnt like being at {characterAge}.")

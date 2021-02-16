272d4fc1a1ca8d23b3e835d1134474559aa5324c
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2021-02-16 20:03:03 +0300

    Add files via upload

---
+++ 10-BMICalculator.py
@@ -0,0 +1,10 @@
+#Don't change the code below
+height = float(input("Enter your height in m : "))
+weight= int(input("Enter your weight in kg: "))
+
+#Don't change the code above
+
+#Write your code below this line
+
+result= weight/height**2
+print(result)

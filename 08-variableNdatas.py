a292f5b712084c17f6f0c29724b3187339e72e1d
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2021-02-16 17:21:38 +0300

    Add files via upload

---
+++ 08-variableNdatas.py
@@ -0,0 +1,7 @@
+character_name ="John"
+character_age =70
+print(f"There is a guy called {character_name} , ")
+print(f"he was {character_age} years old.")
+character_name="Mike"
+print(f"He liked being called as {character_name} ,")
+print(f"but he didnt like being at {character_age}.")

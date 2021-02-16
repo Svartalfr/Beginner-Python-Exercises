11cade3a6d7f13a5510fdd16e4fa9aced8ad5627
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2021-02-16 17:10:01 +0300

    Add files via upload

---
+++ 05-generatebandname.py
@@ -0,0 +1,4 @@
+print("*" * 10 + " "  'Welcome to the band name generator!!! ' + "*"  * 10)
+question1= input("Where were you born in? \n")
+question2=input("What is your pet name? \n ")
+print("Your band name could be : "  + question1  +" "+ question2)

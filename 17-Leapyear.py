7ad1d6d3532e42269711395ab8ccfa720a32e0db
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2021-02-17 03:09:24 +0300

    Add files via upload

---
+++ 17-Leapyear.py
@@ -0,0 +1,15 @@
+print("Welcome to the leap year calculator!")
+
+year=int(input("Which year do you want to check?: "))
+
+if year % 4 ==0:
+    if year % 100 == 0:
+        if year %400==0:
+            print("It is a leap year!")
+        else:
+            print("It is not a leap year!")
+    else:
+        print("It is a leap year!")
+else:
+    print("It is not a leap year!")
+
